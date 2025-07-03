2f2077cf557ab3d4f57857a0aa321b8e1c3b3b62
J M <jm@example.com>
2025-07-02 23:55:52 -0400

    attempt to refactor captcha generation into one spot

--- Cogs/reVerify.py
+++ Cogs/reVerify.py
@@ -12,6 +12,7 @@ from discord import app_commands
 from discord.utils import get
 from PIL import ImageFont, ImageDraw, Image
 from Tools.utils import getConfig
+from Tools.captchaImage import generateCaptcha
 from Tools.logMessage import sendLogMessage
 from loguru import logger
 
@@ -46,91 +47,94 @@ class ReVerifyCog(commands.Cog, name="re-verify"):
             getrole = get(member.guild.roles, id = data["temporaryRole"])
             await member.add_roles(getrole)
             
-            logger.info("Generating captcha")
-            logger.debug("Creating background of captcha")
-            # Create captcha
-            image = np.zeros(shape= (100, 350, 3), dtype= np.uint8)
+            # logger.info("Generating captcha")
+            # logger.debug("Creating background of captcha")
+            # # Create captcha
+            # image = np.zeros(shape= (100, 350, 3), dtype= np.uint8)
 
-            # Create image 
-            image = Image.fromarray(image+255) # +255 : black to white
-            logger.debug("...success! Adding random text")
-            # Add text
-            draw = ImageDraw.Draw(image)
-            font = ImageFont.truetype(font= "Tools/arial.ttf", size= 60)
+            # # Create image 
+            # image = Image.fromarray(image+255) # +255 : black to white
+            # logger.debug("...success! Adding random text")
+            # # Add text
+            # draw = ImageDraw.Draw(image)
+            # font = ImageFont.truetype(font= "Tools/arial.ttf", size= 60)
 
+            # numbers = '23456789' # restricted choices to avoid ambiguous characters
+            # letters = 'ABCDEFGHJKLMNPQRSTUVWXYZ' # restricted choices to avoid ambiguous characters
+            # text = ' '.join(random.choice(numbers + letters) for _ in range(6)) # + string.ascii_lowercase + string.digits
+    
+            # # Center the text
+            # W, H = (350,100)
+            # w = draw.textlength(text, font= font)
+            # h = 45 # magic constant to vertically center the captcha characters in a 100-px space
+            # draw.text(((W-w)/2,(H-h)/2), text, font= font, fill= (90, 90, 90))
+
+            # # Save
+            # logger.debug("...success! Saving temporary image to disk")
+            # ID = member.id
+            # folderPath = f"captchaFolder/{member.guild.id}/captcha_{ID}"
+            # try:
+            #     os.mkdir(folderPath)
+            # except:
+            #     if os.path.isdir(f"captchaFolder/{member.guild.id}") is False:
+            #         os.mkdir(f"captchaFolder/{member.guild.id}")
+            #     if os.path.isdir(folderPath) is True:
+            #         shutil.rmtree(folderPath)
+            #     os.mkdir(folderPath)
+            # image.save(f"{folderPath}/captcha{ID}.png")
+
+            # # Deform
+            # logger.debug("...success! Deforming image")
+            # p = Augmentor.Pipeline(folderPath)
+            # p.random_distortion(probability=1, grid_width=2, grid_height=2, magnitude=35)
+            # p.process()
+
+            # # Search file in folder
+            # path = f"{folderPath}/output"
+            # files = os.listdir(path)
+            # captchaName = [i for i in files if i.endswith('.png')]
+            # captchaName = captchaName[0]
+
+            # image = Image.open(f"{folderPath}/output/{captchaName}")
+            
+            # # Add line
+            # width = random.randrange(10, 15)
+            # co1 = random.randrange(0, 75)
+            # co3 = random.randrange(275, 350)
+            # co2 = random.randrange(20, 50)
+            # co4 = random.randrange(20, 50)
+            # draw = ImageDraw.Draw(image)
+            # draw.line([(co1, co2), (co3, co4)], width= width, fill= (90, 90, 90))
+
+            # # Add another
+            # width = random.randrange(12, 15)
+            # co1 = random.randrange(0, 75)
+            # co3 = random.randrange(275, 350)
+            # co2 = random.randrange(60, 90)
+            # co4 = random.randrange(60, 90)
+            # draw = ImageDraw.Draw(image)
+            # draw.line([(co1, co2), (co3, co4)], width= width, fill= (90, 90, 90))
+            
+            # # Add noise
+            # noisePercentage = 0.60 # 25%
+
+            # pixels = image.load() # create the pixel map
+            # for i in range(image.size[0]): # for every pixel:
+            #     for j in range(image.size[1]):
+            #         rdn = random.random() # Give a random %
+            #         if rdn < noisePercentage:
+            #             pixels[i,j] = (90, 90, 90)
+
+            # # Save
+            # logger.debug("...success! Saving final captcha image to disk")
+            # image.save(f"{folderPath}/output/{captchaName}_2.png")
+
+            # # Send captcha
+            # logger.debug("...success! Sending captcha image to user")
             numbers = '23456789' # restricted choices to avoid ambiguous characters
             letters = 'ABCDEFGHJKLMNPQRSTUVWXYZ' # restricted choices to avoid ambiguous characters
             text = ' '.join(random.choice(numbers + letters) for _ in range(6)) # + string.ascii_lowercase + string.digits
-    
-            # Center the text
-            W, H = (350,100)
-            w = draw.textlength(text, font= font)
-            h = 45 # magic constant to vertically center the captcha characters in a 100-px space
-            draw.text(((W-w)/2,(H-h)/2), text, font= font, fill= (90, 90, 90))
-
-            # Save
-            logger.debug("...success! Saving temporary image to disk")
-            ID = member.id
-            folderPath = f"captchaFolder/{member.guild.id}/captcha_{ID}"
-            try:
-                os.mkdir(folderPath)
-            except:
-                if os.path.isdir(f"captchaFolder/{member.guild.id}") is False:
-                    os.mkdir(f"captchaFolder/{member.guild.id}")
-                if os.path.isdir(folderPath) is True:
-                    shutil.rmtree(folderPath)
-                os.mkdir(folderPath)
-            image.save(f"{folderPath}/captcha{ID}.png")
-
-            # Deform
-            logger.debug("...success! Deforming image")
-            p = Augmentor.Pipeline(folderPath)
-            p.random_distortion(probability=1, grid_width=2, grid_height=2, magnitude=35)
-            p.process()
-
-            # Search file in folder
-            path = f"{folderPath}/output"
-            files = os.listdir(path)
-            captchaName = [i for i in files if i.endswith('.png')]
-            captchaName = captchaName[0]
-
-            image = Image.open(f"{folderPath}/output/{captchaName}")
-            
-            # Add line
-            width = random.randrange(10, 15)
-            co1 = random.randrange(0, 75)
-            co3 = random.randrange(275, 350)
-            co2 = random.randrange(20, 50)
-            co4 = random.randrange(20, 50)
-            draw = ImageDraw.Draw(image)
-            draw.line([(co1, co2), (co3, co4)], width= width, fill= (90, 90, 90))
-
-            # Add another
-            width = random.randrange(12, 15)
-            co1 = random.randrange(0, 75)
-            co3 = random.randrange(275, 350)
-            co2 = random.randrange(60, 90)
-            co4 = random.randrange(60, 90)
-            draw = ImageDraw.Draw(image)
-            draw.line([(co1, co2), (co3, co4)], width= width, fill= (90, 90, 90))
-            
-            # Add noise
-            noisePercentage = 0.60 # 25%
-
-            pixels = image.load() # create the pixel map
-            for i in range(image.size[0]): # for every pixel:
-                for j in range(image.size[1]):
-                    rdn = random.random() # Give a random %
-                    if rdn < noisePercentage:
-                        pixels[i,j] = (90, 90, 90)
-
-            # Save
-            logger.debug("...success! Saving final captcha image to disk")
-            image.save(f"{folderPath}/output/{captchaName}_2.png")
-
-            # Send captcha
-            logger.debug("...success! Sending captcha image to user")
-            captchaFile = discord.File(f"{folderPath}/output/{captchaName}_2.png")
+            captchaFile = await generateCaptcha(member=member, text=text)
             captchaEmbed = await captchaChannel.send(self.bot.translate.msg(member.guild.id, "reVerify", "REVERIFICATION_MESSAGE").format(member.mention), file= captchaFile)
             # Remove captcha folder
             logger.debug("...success! Removing image files on disk")
@@ -147,7 +151,7 @@ class ReVerifyCog(commands.Cog, name="re-verify"):
 
             try:
                 logger.info(f"Starting timer for {member}")
-                msg = await self.bot.wait_for('message', timeout=43200.0, check=check)
+                msg = await self.bot.wait_for('message', timeout=86400.0, check=check)
                 logger.info(f"Message received from {member}, checking captcha")
                 # Check the captcha
                 password = text.split(" ")
---
+++ Tools/captchaImage.py
@@ -0,0 +1,89 @@
+import discord
+import numpy as np
+import random
+import Augmentor
+import os
+import shutil
+from PIL import ImageFont, ImageDraw, Image
+from loguru import logger
+
+async def generateCaptcha(member: discord.Member, text: str):
+    image = np.zeros(shape= (100, 350, 3), dtype= np.uint8)
+
+    # Create image 
+    image = Image.fromarray(image+255) # +255 : black to white
+    logger.debug("...success! Adding random text")
+    # Add text
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype(font= "Tools/arial.ttf", size= 60)
+
+    numbers = '23456789' # restricted choices to avoid ambiguous characters
+    letters = 'ABCDEFGHJKLMNPQRSTUVWXYZ' # restricted choices to avoid ambiguous characters
+    text = ' '.join(random.choice(numbers + letters) for _ in range(6)) # + string.ascii_lowercase + string.digits
+
+    # Center the text
+    W, H = (350,100)
+    w = draw.textlength(text, font= font)
+    h = 45 # magic constant to vertically center the captcha characters in a 100-px space
+    draw.text(((W-w)/2,(H-h)/2), text, font= font, fill= (90, 90, 90))
+
+    # Save
+    logger.debug("...success! Saving temporary image to disk")
+    ID = member.id
+    folderPath = f"captchaFolder/{member.guild.id}/captcha_{ID}"
+    try:
+        os.mkdir(folderPath)
+    except:
+        if os.path.isdir(f"captchaFolder/{member.guild.id}") is False:
+            os.mkdir(f"captchaFolder/{member.guild.id}")
+        if os.path.isdir(folderPath) is True:
+            shutil.rmtree(folderPath)
+        os.mkdir(folderPath)
+    image.save(f"{folderPath}/captcha{ID}.png")
+
+    # Deform
+    logger.debug("...success! Deforming image")
+    p = Augmentor.Pipeline(folderPath)
+    p.random_distortion(probability=1, grid_width=2, grid_height=2, magnitude=35)
+    p.process()
+
+    # Search file in folder
+    path = f"{folderPath}/output"
+    files = os.listdir(path)
+    captchaName = [i for i in files if i.endswith('.png')]
+    captchaName = captchaName[0]
+
+    image = Image.open(f"{folderPath}/output/{captchaName}")
+    
+    # Add line
+    width = random.randrange(10, 15)
+    co1 = random.randrange(0, 75)
+    co3 = random.randrange(275, 350)
+    co2 = random.randrange(20, 50)
+    co4 = random.randrange(20, 50)
+    draw = ImageDraw.Draw(image)
+    draw.line([(co1, co2), (co3, co4)], width= width, fill= (90, 90, 90))
+
+    # Add another
+    width = random.randrange(12, 15)
+    co1 = random.randrange(0, 75)
+    co3 = random.randrange(275, 350)
+    co2 = random.randrange(60, 90)
+    co4 = random.randrange(60, 90)
+    draw = ImageDraw.Draw(image)
+    draw.line([(co1, co2), (co3, co4)], width= width, fill= (90, 90, 90))
+    
+    # Add noise
+    noisePercentage = 0.60 # 60%
+
+    pixels = image.load() # create the pixel map
+    for i in range(image.size[0]): # for every pixel:
+        for j in range(image.size[1]):
+            rdn = random.random() # Give a random %
+            if rdn < noisePercentage:
+                pixels[i,j] = (90, 90, 90)
+
+    # Save
+    logger.debug("...success! Saving final captcha image to disk")
+    image.save(f"{folderPath}/output/{captchaName}_2.png")
+    return f"{folderPath}/output/{captchaName}_2.png"
