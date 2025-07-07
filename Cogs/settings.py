e45f3211fbd910f145c8708b3dcef3b5a57d4ede
J M <jm@example.com>
2025-07-06 22:13:36 -0400

    port setup logic to slash commands

--- Cogs/settings.py
+++ Cogs/settings.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from discord import app_commands
 import enum
 from typing import Optional
+from loguru import logger
 # ------------------------ COGS ------------------------ # 
 class Languages(str, enum.Enum):
     English = "en-US"
@@ -167,13 +168,117 @@ class SettingsCog(commands.Cog, name="settings command"):
             return await inter.response.send_message(embed=embed)
         else:
             # Check that all configuration parameters are set and valid (e.g. roles, channels) before setting captcha true
-            raise NotImplementedError("Feature not yet implemented")
+            # Read configuration.json
+            data = getConfig(inter.guild_id)
+            # Check channel exists
+            configuredChannel = data["captchaChannel"]
+            actualChannel = inter.guild.get_channel(configuredChannel)
+            if actualChannel is None:
+                embed = discord.Embed(title=self.bot.translate.msg(inter.guild_id, "global", "ERROR"),
+                                      description=self.bot.translate.msg(inter.guild_id, "settings", "SETUP_CHANNEL_CHECK_FAILURE"), color=0xe00000) # Red
+                return await inter.response.send_message(embed=embed, ephemeral=True)
+            # Check verification role exists
+            configuredRole = data["temporaryRole"]
+            actualRole = inter.guild.get_role(configuredRole)
+            if actualRole is None:
+                embed = discord.Embed(title=self.bot.translate.msg(inter.guild_id, "global", "ERROR"),
+                                      description=self.bot.translate.msg(inter.guild_id, "settings", "SETUP_ROLE_CHECK_FAILURE"), color=0xe00000) # Red
+                return await inter.response.send_message(embed=embed, ephemeral=True)
+            # If configured, check post-captcha role exists
+            if data["roleGivenAfterCaptcha"] is not "false":
+                configuredRole = data["roleGivenAfterCaptcha"]
+                actualRole = inter.guild.get_role(configuredRole)
+                if actualRole is None:
+                    embed = discord.Embed(title=self.bot.translate.msg(inter.guild_id, "global", "ERROR"),
+                                         description=self.bot.translate.msg(inter.guild_id, "settings", "SETUP_POST_ROLE_CHECK_FAILURE"), color=0xe00000) # Red
+                    return await inter.response.send_message(embed=embed, ephemeral=True)
+            # All checks pass, enable
+            # Add modifications
+            data["captcha"] = True
+            # Save
+            updateConfig(inter.guild_id, data)
+            # Respond
+            embed = discord.Embed(title = self.bot.translate.msg(inter.guild_id, "global", "SUCCESS"), 
+                                  description = self.bot.translate.msg(inter.guild_id, "setup", "CAPTCHA_WAS_SET_UP_WITH_SUCCESS_DESCRIPTION"), color = 0x2fa737) # Green
+            return await inter.response.send_message(embed=embed)
 
-    @config_captcha_group.command(name="setup", description="Configures the captcha protection. Bot attempts to pre-fill empty parameters.")
-    @app_commands.describe(verification_channel = "The channel used to send CAPTCHA challenges and receive responses from users in.")
-    @app_commands.describe(temporary_role = "The temporary role given to users upon join.")
-    async def setup(self, inter: discord.Interaction, verification_channel: Optional[discord.TextChannel] = None, temporary_role: Optional[discord.Role] = None):
-        raise NotImplementedError("Feature not yet implemented")
+
+
+
+    @config_captcha_group.command(name="setup", description="Configures the captcha protection. Bot attempts to create empty parameters.")
+    @app_commands.default_permissions(administrator=True)
+    @app_commands.describe(verification_channel = "The channel used to send and receive CAPTCHA challenges. (Default: bot creates #verification)")
+    @app_commands.describe(temporary_role = "The temporary role given to users upon join. (Default: bot creates \"untested\")")
+    @app_commands.describe(role_after_captcha = "The role to give to users upon successful verification (optional)")
+    @app_commands.describe(log_channel = "The channel to send log messages to. (Default: bot creates #captcha-logs)")
+    async def setup(self, inter: discord.Interaction, verification_channel: Optional[discord.TextChannel] = None, temporary_role: Optional[discord.Role] = None, log_channel: Optional[discord.TextChannel] = None, role_after_captcha: Optional[discord.Role] = None):
+        if isinstance(inter.channel, discord.ForumChannel) or isinstance(inter.channel, discord.CategoryChannel):
+            await inter.response.send_message("This command can only be run from a text channel, please try again.")
+        # Open guild configuration
+        data = getConfig(inter.guild_id)
+        if temporary_role is None:
+            logger.info('Creating the temporary role to be applied to new users')
+            try:
+                temporaryRole = await inter.guild.create_role(name="untested")
+            except discord.Forbidden:
+                embed = discord.Embed(title = self.bot.translate.msg(inter.guild_id, "global", "ERROR"), 
+                                  description = self.bot.translate.msg(inter.guild_id, "setup", "TEMPORARY_ROLE_CREATE_ERROR_DESCRIPTION"), color=0xe00000) # Red
+                return await inter.response.send_message(embed=embed)
+            temporary_role = temporaryRole
+            logger.info("...success!")
+        if verification_channel is None:
+             # Create captcha channel
+            logger.info('Creating verification channel and applying permissions')
+            overwrites = {
+                inter.guild.default_role: discord.PermissionOverwrite(read_messages = False),
+                temporary_role.id: discord.PermissionOverwrite(read_messages = True, send_messages = True) 
+            }
+            try:
+                captchaChannel = await inter.guild.create_text_channel('verification', slowmode_delay=5, overwrites=overwrites)
+            except discord.Forbidden:
+                embed = discord.Embed(title = self.bot.translate.msg(inter.guild_id, "global", "ERROR"), 
+                                  description = self.bot.translate.msg(inter.guild_id, "setup", "TEMPORARY_CHANNEL_CREATE_ERROR_DESCRIPTION"), color=0xe00000) # Red
+                return await inter.response.send_message(embed=embed)
+            verification_channel = captchaChannel
+            logger.info("...success!")
+        if log_channel is None:
+             # Create log channel
+            logger.info('Creating log channel and applying permissions')
+            try:
+                logChannel = await inter.guild.create_text_channel('captcha-logs', overwrites={inter.guild.default_role: discord.PermissionOverwrite(read_messages = False)})
+            except discord.Forbidden:
+                embed = discord.Embed(title = self.bot.translate.msg(inter.guild_id, "global", "ERROR"), 
+                                  description = self.bot.translate.msg(inter.guild_id, "setup", "TEMPORARY_CHANNEL_CREATE_ERROR_DESCRIPTION"), color=0xe00000) # Red
+                return await inter.response.send_message(embed=embed)
+            log_channel = logChannel
+            logger.info("...success!")
+        # by this point both verification_channel and temporary_role should be valid objects of the Role and TextChannel types
+        logger.info('Hiding all channels from the temporary role')
+        await inter.response.defer()
+        missedChannels = []
+
+        try:
+            for channel in inter.guild.channels:
+                logger.debug(f'Starting to override the permissions for {channel}.')
+                await channel.set_permissions(temporary_role, overwrite=discord.PermissionOverwrite(read_messages = False))
+        except discord.Forbidden as error:
+                            logger.error(f"Failed to change permissions (likely missing access to channel {channel} ({channel.id}))")
+                            missedChannels.append(channel)
+        if len(missedChannels) > 0:
+            errors = ", ".join(missedChannels)
+            embed = discord.Embed(title=self.bot.translate.msg(inter.guild_id, "setup", "CHANNEL_ACCESS_WARNING"), description=self.bot.translate.msg(inter.guild_id, "setup", "CHANNEL_ACCESS_WARNING_DESCRIPTION").format(errors), color=0xffff00) # Yellow
+            await inter.channel.send(embed=embed)
+
+        # Edit configuration.json
+        # Add modifications
+        logger.debug('Saving data to configuration')
+        data["captcha"] = True
+        data["temporaryRole"] = temporary_role.id
+        data["captchaChannel"] = verification_channel.id
+        data["logChannel"] = log_channel.id
+
+
+
         
 
 
