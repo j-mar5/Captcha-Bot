652139ab03797f92a434ccc6b2f80219d63f4416
J M <jm@example.com>
2025-07-09 00:29:52 -0400

    more troubleshooting command rejection by discord

--- Cogs/help.py
+++ Cogs/help.py
@@ -15,7 +15,7 @@ class HelpCog(commands.Cog):
                           description="Show the help page.")
     @app_commands.guild_only()
     @app_commands.default_permissions(manage_messages = True)
-    async def help(self, inter: discord.Interaction, commandName: Optional[str] = None):
+    async def help(self, inter: discord.Interaction, commandName: str):
 
         commandName2 = None
         stop = False
